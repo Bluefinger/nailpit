ddc1b44919c0390d64f09acf03c3032cb7f5030b
Gonçalo Rica Pais da Silva <bluefinger@gmail.com>
2025-06-02 17:11:43 +0200

    Add toggleable logging/traces

--- src/main.rs
+++ src/main.rs
@@ -36,16 +36,24 @@ async fn nailpit_main(
 ) -> Result<()> {
     logforth::builder()
         .dispatch(|d| {
-            d.filter(EnvFilter::from_default_env())
-                .diagnostic(logforth::diagnostic::FastraceDiagnostic::default())
-                .append(logforth::append::FastraceEvent::default())
-                .append(nailpit::otel::init_logging_reporter(config.as_ref()))
-                .append(append::Stderr::default())
+            let d = d.filter(EnvFilter::from_default_env());
+
+            if config.open_telemetry.logs {
+                d
+                    .diagnostic(logforth::diagnostic::FastraceDiagnostic::default())
+                    .append(logforth::append::FastraceEvent::default())
+                    .append(nailpit::otel::init_logging_reporter(config.as_ref()))
+                    .append(append::Stderr::default())
+            } else {
+                d.append(append::Stderr::default())
+            }
         })
         .apply();
 
     #[cfg(feature = "tracing")]
-    nailpit::otel::init_tracing_reporter(config.as_ref());
+    if config.open_telemetry.traces {
+        nailpit::otel::init_tracing_reporter(config.as_ref());
+    }
 
     log::info!("Welcome to Nailpit!");
     log::info!("Loaded config: {config:?}");
